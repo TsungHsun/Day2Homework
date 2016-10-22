87f37eae7baa975608927ab9aa074aa8b1fcbc63
TsungHsun <fb282@yahoo.com.tw>
2016-10-22 13:54:03 +0800

    Add Scenario 4

--- Day2-Homework/Day2-HomeworkTests1/PotterShoppingCartTests.cs
+++ Day2-Homework/Day2-HomeworkTests1/PotterShoppingCartTests.cs
@@ -63,5 +63,28 @@ namespace Day2_Homework.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CheckoutTest_一二三四集各買了一本_價格應為320()
+        {
+            PotterShoppingCart target = new PotterShoppingCart();
+            int qty = 1;
+
+            int episode;
+            episode = 1;
+            target.AddItem(episode, qty);
+            episode = 2;
+            target.AddItem(episode, qty);
+            episode = 3;
+            target.AddItem(episode, qty);
+            episode = 4;
+            target.AddItem(episode, qty);
+            int expected = 320;
+
+            target.Checkout();
+            int actual = target.Amount;
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
