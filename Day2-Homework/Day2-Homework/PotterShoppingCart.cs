27a0bdc1c4b17fb445f601027e3df020fad94485
TsungHsun <fb282@yahoo.com.tw>
2016-10-22 13:20:49 +0800

    Add Scenario 1

---
+++ Day2-Homework/Day2-Homework/PotterShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2_Homework
+{
+    public class PotterShoppingCart
+    {
+        public int Amount { get; set; }
+
+        public void AddItem(int episode, int qty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Checkout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day2-Homework/Day2-HomeworkTests1/PotterShoppingCartTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2_Homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2_Homework.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void CheckoutTest_第一集買了一本_其他都沒買_價格應為100()
+        {
+            PotterShoppingCart target = new PotterShoppingCart();
+            int episode = 1;
+            int qty = 1;
+            target.AddItem(episode, qty);
+            int expected = 100;
+
+            target.Checkout();
+            int actual = target.Amount;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
