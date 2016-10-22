7eaf8df003dc14871ec6f4f00eed7df773160d93
TsungHsun <fb282@yahoo.com.tw>
2016-10-22 13:31:10 +0800

    Pass Scenario 1

--- Day2-Homework/Day2-Homework/PotterShoppingCart.cs
+++ Day2-Homework/Day2-Homework/PotterShoppingCart.cs
@@ -7,16 +7,18 @@ namespace Day2_Homework
 {
     public class PotterShoppingCart
     {
+        private int[] _qty = new int[5];
+
         public int Amount { get; set; }
 
         public void AddItem(int episode, int qty)
         {
-            throw new NotImplementedException();
+            this._qty[episode - 1] += qty;
         }
 
         public void Checkout()
         {
-            throw new NotImplementedException();
+            this.Amount = _qty.Sum() * 100;
         }
     }
 }
